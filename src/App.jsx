29688370b4075340f64489ef4b679df1460a20f8
Thabo <thvbojafta@gmail.com>
2024-06-14 21:03:23 +0200

    Link Logo to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
